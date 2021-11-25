65a5966bb6e23887c7c3576437960b76355c519e
Diana-Banana <DianaStrezhesku@gmail.com>
2021-11-24 20:00:37 -0500

    added extra method addNum

--- src/packageBanana/ClassBanana.java
+++ src/packageBanana/ClassBanana.java
@@ -4,5 +4,9 @@ public class ClassBanana {
 	public static void main(String[] args) {
 		System.out.println("BananaDianaBananaDiana");
 	}
+	
+	public static void addNum(int a, int b) {
+		System.out.println(a + b);
+	}
 
 }
