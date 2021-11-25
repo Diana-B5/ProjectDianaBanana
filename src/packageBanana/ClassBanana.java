3a12253746dfdb13a34e54eeda1d84628004c9ca
Diana-B5 <94864031+Diana-B5@users.noreply.github.com>
2021-11-24 20:43:16 -0500

    Update ClassBanana.java

--- src/packageBanana/ClassBanana.java
+++ src/packageBanana/ClassBanana.java
@@ -3,6 +3,8 @@ package packageBanana;
 public class ClassBanana {
 	public static void main(String[] args) {
 		System.out.println("BananaDianaBananaDiana");
+		
+		System.out.println("Pull this, added from the gitHub");
 	}
 	
 	public static void addNum(int a, int b) {
